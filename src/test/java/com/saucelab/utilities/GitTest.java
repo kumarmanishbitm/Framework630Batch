37e6ae208f16804eb00349dfbe4e51d26034a68c
kumarmanishbitm <kumarmanishbitm@gmail.com>
2023-06-22 07:19:06 +0530

    This is Second Commit

---
+++ src/test/java/com/saucelab/utilities/GitTest.java
@@ -0,0 +1,11 @@
+package com.saucelab.utilities;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is GIT changes class for new commit and Push.");
+
+	}
+
+}
